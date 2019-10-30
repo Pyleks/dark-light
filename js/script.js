1100fc4d67fb55262ab8cc6513c03a3220acb21d
Pyleks <aleksander.andersen@outlook.ie>
2019-10-30 11:06:32 +0000

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $("a").mouseover(function() {
+        $(this).addClass("makeYellow")
+    });
+    $("a").mouseleave(function() {
+        $(this).removeClass("makeYellow")
+    });
+
+    $(".white-square").mouseover(function() {
+        $("html").css("background-color", "white");
+        $("body").css("background-color", "white");
+        $(".menu-bar-dark").css("background-color", "white");
+        $("h1").css("color", "black");
+        $("h2").css("color", "black");
+        $("h1").html("LIGHT");
+
+
+    });
+
+    $(".black-square").mouseover(function() {
+        $("html").css("background-color", "black");
+        $("body").css("background-color", "black");
+        $(".menu-bar-dark").css("background-color", "black");
+        $("h1").css("color", "white");
+        $("h2").css("color", "white");
+        $("h1").html("DARK");
+    });
+})
