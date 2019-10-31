0395b9e77dc60c41af815e3f075c95c23affac59
Pyleks <aleksander.andersen@outlook.ie>
2019-10-31 22:26:20 +0000

    Added more pages and more Scripts (Will optimize the scripts later)

---
+++ js/about.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    if(localStorage.getItem('black') === 'clicked'){
+        $("html").css("background-color", "black");
+        $("body").css("background-color", "black");
+        $(".menu-bar-dark").css("background-color", "black");
+        localStorage.removeItem('black');
+    } else {
+        $("html").css("background-color", "white");
+        $("body").css("background-color", "white");
+        $(".menu-bar-dark").css("background-color", "white");
+        $("h1").css("color", "black");
+        $("h2").css("color", "black");
+        $("#about-dl-h1").html("ABOUT LIGHT");
+        localStorage.removeItem('white');
+    }
+})
--- js/script.js
+++ js/script.js
@@ -1,4 +1,34 @@
 $(document).ready(function(){
+    $("#new_page_style_about").on("click",function(){
+        if($("body").css("background-color") === "rgb(0, 0, 0)") {
+            localStorage.setItem("black", "clicked")
+            window.location.href = "about-dl.html";
+        } else {
+            localStorage.setItem("white", "clicked")
+            window.location.href = "about-dl.html";
+    }
+});
+    $("#new_page_style_project").on("click",function(){
+        if($("body").css("background-color") === "rgb(0, 0, 0)") {
+            localStorage.setItem("black", "clicked")
+            window.location.href = "project.html";
+        } else {
+            localStorage.setItem("white", "clicked")
+            window.location.href = "project.html";
+        }
+    });
+
+    $("#new_page_style_technologies").on("click",function(){
+        if($("body").css("background-color") === "rgb(0, 0, 0)") {
+            localStorage.setItem("black", "clicked")
+            window.location.href = "technologies.html";
+        } else {
+            localStorage.setItem("white", "clicked")
+            window.location.href = "technologies.html";
+        }
+    });
+
+
     $(".nav-text-style").mouseover(function() {
         $(this).addClass("makeYellow")
     });
@@ -12,7 +42,10 @@ $(document).ready(function(){
         $(".menu-bar-dark").css("background-color", "white");
         $("h1").css("color", "black");
         $("h2").css("color", "black");
-        $("h1").html("LIGHT");
+        $("#landing-h1").html("LIGHT");
+        $("#about-dl-h1").html("ABOUT LIGHT");
+        $("#project-headline").html("PROJECT LIGHT")
+        $("#technology-headline").html("TECHNOLOGY LIGHT")
     });
     $(".black-square").mouseover(function() {
         $("html").css("background-color", "black");
@@ -20,6 +53,17 @@ $(document).ready(function(){
         $(".menu-bar-dark").css("background-color", "black");
         $("h1").css("color", "white");
         $("h2").css("color", "white");
-        $("h1").html("DARK");
+        $("#landing-h1").html("DARK");
+        $("#about-dl-h1").html("ABOUT DARK");
+        $("#project-headline").html("PROJECT DARK")
+        $("#technology-headline").html("TECHNOLOGY DARK")
     });
+    $("body").click(function() {
+        if($(this).css("background-color") === "rgb(0, 0, 0)") {
+            console.log("This is Black");
+        } else {
+            console.log("This is White")
+        }
+    })
 })
+
